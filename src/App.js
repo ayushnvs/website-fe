4e19b4ab0609cf6f846cfefdcdcf08c585a5a2a6
ayushnvs <ayush.mishra.ald@gmail.com>
2023-11-27 18:35:50 +0530

    Add login & registration page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './navbar'
+import LoginPage from './loginPage'
+import RegistrationPage from './registrationPage'
+import Footer from './footer'
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/login' element={<LoginPage/>} />
+          <Route path='/register' element={<RegistrationPage/>} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/registrationPage.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const RegistrationPage = () => {
+  return (
+    <div className='bg-body-tertiary pb-4 pt-4'>
+      <div className="nav justify-content-center pt-4 mb-4">
+        <img className='nav-item' src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width={72} height={57} />
+      </div>
+      <div className="nav justify-content-center h3">welcome to Narayana blogs</div>
+      <p className="nav justify-content-center h5 mt-4 fw-normal">Please register</p>
+      <div className='d-flex align-items-center'>
+        <main className="form-signin w-100 m-auto">
+          <form>
+            <div className="form-floating">
+              <input type="text" className="form-control" id="floatingInput" placeholder="username" />
+              <label htmlFor="floatingInput">Username</label>
+            </div>
+            <div className="form-floating">
+              <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+              <label htmlFor="floatingPassword">Password</label>
+            </div>
+            <div className="form-check text-start my-3">
+              <input className="form-check-input" type="checkbox" defaultValue="remember-me" id="flexCheckDefault" />
+              <label className="form-check-label" htmlFor="flexCheckDefault">
+                Remember me
+              </label>
+            </div>
+            <button className="btn btn-primary w-100 py-2" type="submit">
+              Register
+            </button>
+            <div className='mt-4 mb-4'>Already have an account? <a href="/login">Login here</a></div>
+          </form>
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default RegistrationPage

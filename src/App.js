49690a8d90a25bf2ee2206e60c4a92c9e461c755
ayushnvs <ayush.mishra.ald@gmail.com>
2023-12-03 23:29:23 +0530

    Modify layout and app

--- src/App.js
+++ src/App.js
@@ -1,47 +1,12 @@
-import { useEffect, useRef } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './components/login/login'
 import RegistrationPage from './components/register/registration'
 import Home from './components/home/home'
 import Profile from './components/profile/profile'
 import Layout from './layout/layout'
-import useAuth from './hooks/AuthProvider'
 import RequireAuth from './hooks/RequireAuth'
-import axios from "axios"
-
-const useOptimizedComponentWillMount = callback => {
-    const mounted = useRef(false)
-    if (!mounted.current) callback()
-
-    useEffect(() => {
-        mounted.current = true
-    }, []);
-};
 
 const App = () => {
-    const { setAuth } = useAuth()
-
-
-    useOptimizedComponentWillMount(() => {
-        const username = localStorage.getItem('username');
-        const token = localStorage.getItem('token')
-
-        if (!!username && !!token) {
-            try {
-                (async () => {
-                    const res = await axios.get(`http://localhost:8000/account/${username}`)
-                    setAuth({
-                        ...res.data,
-                        token,
-                        isLoggedIn: true
-                    })
-                })()
-            } catch (err) {
-                // Handle errors
-            }
-        }
-
-    }, [])
 
     return (
         <div className='App'>
--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,17 +1,53 @@
 import { Outlet } from "react-router-dom"
+import { useState, useEffect } from "react"
 import Navbar from './../components/navigation/navbar'
 import Footer from './../components/navigation/footer'
 import useAuth from "../hooks/AuthProvider"
+import axios from "axios"
 
 const Layout = () => {
     const { auth, setAuth } = useAuth()
+    const [ isLoading, setIsLoading ] = useState(true)
+
+    useEffect(() => {
+        const verifyToken = async () => {
+            const username = localStorage.getItem('username');
+            const token = localStorage.getItem('token')
+            if (!!username && !!token) {
+                try {
+                    const res = await axios.get(`http://localhost:8000/account/${username}`)
+                    setAuth({
+                        ...res.data,
+                        token,
+                        isLoggedIn: true
+                    })
+                } catch (err) {
+                    console.log(err)
+                }
+                finally {
+                    setIsLoading(false)
+                }
+            } else {
+                setIsLoading(false)
+            }
+        }
+
+        !auth.token ? verifyToken() : setIsLoading(false)
+    }, [])
 
     return (
         <main className="App">
-            {  console.log('AUTH', auth)  }
-            <Navbar />
-            <Outlet />
-            <Footer />
+            {
+                isLoading
+                    ? <>
+                        <p className="loadin">Loadin...</p>
+                    </>
+                    : <>
+                        <Navbar />
+                        <Outlet />
+                        <Footer />
+                    </>
+            }
         </main>
     )
 }

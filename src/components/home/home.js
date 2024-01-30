af4a4d9cb092238c49a8fa82efd1a5af5feaa24a
akankshavns <makanksha858@gmail.com>
2024-01-30 21:36:12 +0530

    Add snippet for blog card

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,44 +1,11 @@
 import { Link } from 'react-router-dom'
-import './home.css'
+import BlogCard from '../snippets/homeBlogCard/blogCard'
 
 const Home = () => {
 
   return (
     <>
-      <div className="container-2">
-        <div className="one" />
-        {/* <nav className="link red">
-          <a href="" classname="link">
-            hello
-          </a>
-          <a href="" className="link">
-            hello
-          </a>
-          <a href="">hello </a>
-          <a href=""> hello</a>
-        </nav> */}
-        <div className="two">
-          <img
-            src="https://img.freepik.com/free-photo/business-women-signature-document_1388-90.jpg?w=740&t=st=1706629443~exp=1706630043~hmac=b7b7c8756f325f42ff15e0a9ce7f57d4e40a9885a49a25b25947136226882844"
-            alt="image"
-            width="650px"
-          />
-          <div className="text-part">
-            <div className="red">
-              <div className="green">
-                <div className="red">
-                  <a href="green">
-                    <img src="" alt="" />
-                  </a>
-                  <div />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="" />
-        </div>
-        <div className="three" />
-      </div>
+     <BlogCard />
 
     </>
   )

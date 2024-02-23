92736ec6cda35346718647c1b39949048cb8f447
ayushnvs <ayush.mishra.ald@gmail.com>
2024-02-23 18:03:31 +0530

    Update Login Page

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -3,9 +3,10 @@ import { Link, useNavigate, useLocation } from 'react-router-dom'
 import axios from '../../api/axios'
 import useAuth from '../../hooks/AuthProvider'
 import Logo from './../../assets/img/Conch1.jpg'
+import registerImg from './../../assets/img/register.jpeg'
 
 const LoginPage = () => {
-    const {setAuth } = useAuth()
+    const { setAuth } = useAuth()
     const navigate = useNavigate()
     const location = useLocation()
     const from = location.state?.from?.pathname || '/'
@@ -20,10 +21,10 @@ const LoginPage = () => {
 
     const handleShowPassword = e => {
         let showPasswordInput = passwordRef.current
-        if (showPassword) {   
+        if (showPassword) {
             showPasswordInput.setAttribute('type', "");
         } else showPasswordInput.setAttribute('type', "password");
-        
+
         setShowPassword(() => {
             return !showPassword
         })
@@ -43,7 +44,7 @@ const LoginPage = () => {
 
         if (username !== "" && password !== "") {
             try {
-                const res = await axios.post('/account/login', user, {withCredentials: true })
+                const res = await axios.post('/account/login', user, { withCredentials: true })
                 console.log(res)
                 setAuth({
                     token: res.data.token,
@@ -68,33 +69,48 @@ const LoginPage = () => {
                 <img className='nav-item site-logo' src={Logo} alt="" width={72} height={57} />
             </div>
             <div className="nav justify-content-center h3">welcome to Narayana blogs</div>
-            <p className="nav justify-content-center h5 mt-4 fw-normal">Please sign in</p>
-            <div className='d-flex align-items-center'>
-                <main className="form-signin w-100 m-auto">
-                    <form onSubmit={handleSubmit}>
-                        <div className="mb-3">
-                            <label htmlFor="Input" className="form-label">Username</label>
-                            <span className="text-danger">*</span>
-                            <input type="text" className="form-control" id="Input" name="username" onChange={handleChange} />
-                        </div>
-                        <div>
-                            <label htmlFor="Password" className="form-label">Password</label>
-                            <span className="text-danger">*</span>
-                            <input ref={passwordRef} type="password" className="form-control" id="Password" name="password" onChange={handleChange} />
-                        </div>
-                        <div className="form-check text-start my-3">
-                            <input className="form-check-input" type="checkbox" defaultValue="remember-me" id="flexCheckDefault" onClick={handleShowPassword} />
-                            <label className="form-check-label" htmlFor="flexCheckDefault">
-                                Show Password
-                            </label>
-                        </div>
-                        <button className="btn btn-primary w-100 py-2" type="submit">
-                            Sign in
-                        </button>
-                        <div className='mt-4 mb-4'>Don't have an account? <Link to="/register">Register here</Link></div>
-                    </form>
-                </main>
+            {/* <p className="nav justify-content-center h5 mt-4 fw-normal">Please sign in</p> */}
+            <div className="row">
+                <div className="col-7">
+                    <div className='d-flex align-items-center mt-4'>
+                        <main className="form-signin w-100 m-auto">
+                            <form onSubmit={handleSubmit}>
+                                <div className="mb-3">
+                                    <label htmlFor="Input" className="form-label">Username</label>
+                                    <span className="text-danger">*</span>
+                                    <input type="text" className="form-control" id="Input" name="username" onChange={handleChange} />
+                                </div>
+                                <div>
+                                    <label htmlFor="Password" className="form-label">Password</label>
+                                    <span className="text-danger">*</span>
+                                    <input ref={passwordRef} type="password" className="form-control" id="Password" name="password" onChange={handleChange} />
+                                </div>
+                                <div className="form-check text-start my-3">
+                                    <input className="form-check-input" type="checkbox" defaultValue="remember-me" id="flexCheckDefault" onClick={handleShowPassword} />
+                                    <label className="form-check-label" htmlFor="flexCheckDefault">
+                                        Show Password
+                                    </label>
+                                </div>
+                                <button className="btn btn-primary w-100 py-2" type="submit">
+                                    Sign in
+                                </button>
+                                <div className='mt-4 mb-4'>Don't have an account? <Link to="/register">Register here</Link></div>
+                            </form>
+                        </main>
+                    </div>
+                </div>
+                <div className="col-5">
+                    <div className="nav justify-content-center mt-4 pt-4">
+                        <img className="nav-item"
+                            src={registerImg}
+                            alt=""
+                            width={300}
+                            height={200}
+                        />
+                    </div>
+                </div>
             </div>
+
         </div>
     )
 }
